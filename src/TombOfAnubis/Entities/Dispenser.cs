c506b4880dcd64191e5e9b79dcf005bce9c43a74
Justin Studer <jstuder@student.ethz.ch>
2023-04-11 16:50:02 +0200

    Re-enabled speedup

--- src/TombOfAnubis/Entities/Dispenser.cs
+++ src/TombOfAnubis/Entities/Dispenser.cs
@@ -55,7 +55,7 @@ namespace TombOfAnubis
             if(dispenserType == DispenserType.BodyPowerup)
             {
 
-                switch(random.Next(1, 1))
+                switch(random.Next(0, 2))
                 {
                     case 0:
                         emptyItemSlot.Item = new InventoryItem(ItemType.Speedup, emptyItemSlot.Entity);
