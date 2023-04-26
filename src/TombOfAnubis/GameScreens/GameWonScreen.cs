5622cdc8f54d7dbe1ddb465021246252945088d1
Hrishikesh Ghodki <ghodkih@student.ethz.ch>
2023-04-27 01:10:50 +0200

    Deferred skipping of GameWon video

--- src/TombOfAnubis/GameScreens/GameWonScreen.cs
+++ src/TombOfAnubis/GameScreens/GameWonScreen.cs
@@ -17,10 +17,16 @@ namespace TombOfAnubis
         private Color statusColor = Color.Gold;
         private float fontScale = 1f;
 
+        // In milliseconds
+        private int cooldownPeriod = 2000;
+        private double videoStartTime;
+        private bool skipCooldown = false;
+        private bool startTimeSet = false;
+
         public GameWonScreen()
             : base()
         {
-
+            
         }
         public override void LoadContent()
         {
@@ -31,6 +37,17 @@ namespace TombOfAnubis
         public override void Update(GameTime gameTime, bool otherScreenHasFocus,
                                                        bool coveredByOtherScreen)
         {
+            if (!startTimeSet)
+            {
+                startTimeSet = true;
+                videoStartTime = gameTime.TotalGameTime.TotalMilliseconds;
+            }
+            double diff = gameTime.TotalGameTime.TotalMilliseconds - videoStartTime;
+            if (diff >  cooldownPeriod)
+            {
+                skipCooldown = true;
+            }
+
             base.Update(gameTime, otherScreenHasFocus, coveredByOtherScreen);
 
         }
@@ -38,7 +55,7 @@ namespace TombOfAnubis
         {
             foreach (PlayerInput playerInput in InputController.PlayerInputs)
             {
-                if (playerInput.UseTriggered())
+                if (playerInput.UseTriggered() && skipCooldown)
                 {
 
                     InputController.AddCooldown(playerInput.UseKey, playerInput.UseButton, 250);
@@ -55,15 +72,19 @@ namespace TombOfAnubis
             Viewport viewport = GameScreenManager.GraphicsDevice.Viewport;
             //var videoTexture = videoPlayer.GetTexture();
             VideoController.Draw(spriteBatch, new Rectangle(viewport.X, viewport.Y, viewport.Width, viewport.Height));
+            
+            if (skipCooldown)
+            {
+                string statusText = "Press [E] / (A)";
+                Vector2 textLength = statusFont.MeasureString(statusText) * fontScale;
 
-            string statusText = "Press [E] / (A)";
-            Vector2 textLength = statusFont.MeasureString(statusText) * fontScale;
+                Vector2 displayPosition = new Vector2(viewport.X + viewport.Width * 4f / 5f, viewport.Y + viewport.Height * 4f / 5f);
+                spriteBatch.Begin();
+                spriteBatch.DrawString(statusFont, statusText, displayPosition, statusColor,
+                0f, Vector2.Zero, fontScale, SpriteEffects.None, 0f);
+                spriteBatch.End();
+            }
 
-            Vector2 displayPosition = new Vector2(viewport.X + viewport.Width * 4f / 5f, viewport.Y + viewport.Height * 4f / 5f);
-            spriteBatch.Begin();
-            spriteBatch.DrawString(statusFont, statusText, displayPosition, statusColor,
-            0f, Vector2.Zero, fontScale, SpriteEffects.None, 0f);
-            spriteBatch.End();
         }
     }
 }
