1c8b87375ca56d83cc6519dffd4f06f9043507ee
boelukas <boelukas@student.ethz.ch>
2023-04-27 15:02:41 +0200

    Add particle effects back to anubis

--- src/TombOfAnubis/MapGenerator/EntityGenerator.cs
+++ src/TombOfAnubis/MapGenerator/EntityGenerator.cs
@@ -99,14 +99,45 @@ namespace TombOfAnubis
         }
         public static Entity SpawnAnubis(EntityDescription entityDescription)
         {
+            ParticleEmitterConfiguration pec = new ParticleEmitterConfiguration();
+            pec.LocalPosition = new Vector2(25f, 100f);
+            pec.RandomizedSpawnPositionRadius = 50f;
+            //doesn't work yet
+            pec.ParticlesMoveWithEntity = false;
+            pec.Texture = ParticleTextureLibrary.BasicParticle;
+            pec.SpriteLayer = 1;
+            pec.InitialAlpha = 0.5f;
+            pec.AlphaMode = AlphaMode.LinearDecreaseToZero;
+            pec.RandomizedTintMin = Color.SlateGray;
+            pec.RandomizedTintMax = Color.DimGray;
+            pec.Scale = Vector2.One * 0.4f;
+            pec.ScalingMode = ScalingMode.Constant;
+            pec.RelativeScaleVariation = new Vector2(0.8f, 0.8f);
+            pec.EmitterDuration = 0f;
+            pec.ParticleDuration = 2f;
+            pec.EmissionFrequency = 60f;
+            pec.EmissionRate = 1f;
+            pec.InitialSpeed = 50f;
+            pec.SpawnDirection = new Vector2(0f, -1f);
+            pec.SpawnConeDegrees = 90f;
+            pec.Gravity = new Vector2(0f, 0f);
+            //currently behaves a bit unintuitively
+            pec.LocalPointForcePosition = Vector2.Zero;
+            pec.PointForceStrength = 0f;
+            pec.PointForceUsesQuadraticFalloff = false;
+            pec.Gravity = new Vector2(0f, 0f);
+            pec.Drag = 0.5f;
+
             entityDescription.Load(content, @"Textures\Characters");
-            return new Anubis(
+            Anubis anubis = new Anubis(
                         Session.GetInstance().Map.CreateEntityTileCenteredPosition(entityDescription),
                         entityDescription.Scale,
                         entityDescription.Texture,
                         entityDescription.Animation,
                         Session.GetInstance().Map.EntityProperties.MaxAnubisMovementSpeed,
                         Session.GetInstance().Map);
+            anubis.AddComponent(new ParticleEmitter(pec));
+            return anubis;
         }
         public static Entity SpawnAltar(EntityDescription entityDescription)
         {
